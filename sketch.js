1867910b8cf39ca2011c52b33608c5420ea803b6
ProfaTatianeTobias <93044628+ProfaTatianeTobias@users.noreply.github.com>
2022-10-06 21:16:19 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,58 +1,70 @@
 var canvas;
-var backgroundImage
-var powerCoins
-var coinIMG
-var life,lifeIMG
-var personagemPrincipal
-var enimes
+var backgroundIMG;
+var powerCoins;
+var coinIMG;
+var life,lifeIMG;
+var personagemPrincipal, personagemPrincipalIMG, personagem;
+var enimes;
+var fundo, fundo2;
 
 function preload() {
-  coinIMG=loadImage("./assets/goldCoin.png")
-  lifeIMG=loadImage("./assets/life.png")
-
+  coinIMG=loadImage("./assets/goldCoin.png");
+  lifeIMG=loadImage("./assets/life.png");
+  backgroundIMG=loadImage("./assets/fundo1.png");
+  personagemPrincipalIMG=loadImage("./assets/menino.gif");
 }
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
-  personagemPrincipal=createSprite(100,height-100)
-  personagemPrincipal.shapeColor="blue"
-  enimes=new Group()
+
+  fundo = createSprite(width/2, height/2);
+  fundo.scale = 1;
+  fundo.addImage(backgroundIMG);
+
+  personagemPrincipal=createSprite(100,100);
+  personagemPrincipal.shapeColor="blue";
+  personagemPrincipal.addImage(personagemPrincipalIMG);
+  personagemPrincipal.scale = 5;
+
+  personagem=createSprite(500,500);
+  personagem.shapeColor="purple";
+  enimes=new Group();
+
 }
 
 function draw() {
-  background("white");
+  background("gray");
+ 
   move()
   drawEnimes()
-  personagemPrincipal.velocityX=100
-  //camera.position.x=personagemPrincipal.x
-  //camera.position.y=personagemPrincipal.y
+  
+  camera.position.x=personagemPrincipal.x
+  camera.position.y=personagemPrincipal.y
   drawSprites()
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
 function drawEnimes(){
 if(frameCount%200===0){
-var enime
-enime =createSprite(width-100,height-100 )
-enime.shapeColor="red"
-enime.velocityX=-2
-enimes.add(enime)
+var enime;
+enime =createSprite(width-100,height-100);
+enime.shapeColor="red";
+enime.velocityX=-2;
+enimes.add(enime);
 }
 }
-  function move(){
+
+function move(){
 if(keyIsDown(UP_ARROW)){
-personagemPrincipal.y-=1
+personagemPrincipal.y-=10
 console.log("funcionou")
 }
 if(keyIsDown(DOWN_ARROW)){
-personagemPrincipal.y+=1
+personagemPrincipal.y+=10
  }
  if(keyIsDown(LEFT_ARROW)){
-personagemPrincipal.x-=1
+personagemPrincipal.x-=10
 }if(keyIsDown(RIGHT_ARROW)){
-  personagemPrincipal.x+=1
+  personagemPrincipal.x+=10
   }
 
 }
